6c5c4036a4546086ecfcef1812c504bbe87b337b
EC2 Default User <ec2-user@ip-172-31-18-93.ec2.internal>
2022-05-19 22:21:00 +0000

    :horse: いいね機能を非同期化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,16 +5,14 @@ class FavoritesController < ApplicationController
   end
 
   def create
-    theme = Theme.find(params[:theme_hashid])
-    favorite = current_user.favorites.new(theme_id: theme.id)
+    @theme = Theme.find(params[:theme_hashid])
+    favorite = current_user.favorites.new(theme_id: @theme.id)
     favorite.save
-    redirect_to theme_path(user_name: theme.user.name, theme_hashid: theme.hashid)
   end
 
   def destroy
-    theme = Theme.find(params[:theme_hashid])
-    favorite = current_user.favorites.find_by(theme_id: theme.id)
+    @theme = Theme.find(params[:theme_hashid])
+    favorite = current_user.favorites.find_by(theme_id: @theme.id)
     favorite.destroy
-    redirect_to theme_path(user_name: theme.user.name, theme_hashid: theme.hashid)
   end
 end
