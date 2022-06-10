95616557c5a5202bea154476451531177d8fe6ad
EC2 Default User <ec2-user@ip-172-31-18-93.ec2.internal>
2022-06-10 08:50:33 +0000

    :art: Modal Formの整形

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -11,7 +11,7 @@ class Theme < ApplicationRecord
   # has_many :post_tags, dependent: :destroy
   # has_many :tags, through: :post_tags, source: :tag
 
-  validates :title, presence: true, length: { maximum: 30 }
+  validates :title, presence: true, length: { maximum: 50 }
 
   include Hashid::Rails
 
