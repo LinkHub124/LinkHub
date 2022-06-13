264ea2d7970438ea5155743ea998e6c28cff6f9e
EC2 Default User <ec2-user@ip-172-31-18-93.ec2.internal>
2022-06-13 07:38:38 +0000

    :art: レイアウトの修正

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -15,16 +15,16 @@ class LinksController < ApplicationController
     @theme    = Theme.find(params[:theme_hashid])
     ret_link_params = link_params
     if ret_link_params[:one_links_attributes].blank? == false
-    ret_link_params[:one_links_attributes].keys.each { |idx|
-      url = ret_link_params[:one_links_attributes][idx][:url]
-      if ret_link_params[:one_links_attributes][idx][:_destroy] != nil
-        next
-      end
+      ret_link_params[:one_links_attributes].keys.each { |idx|
+        url = ret_link_params[:one_links_attributes][idx][:url]
+        if ret_link_params[:one_links_attributes][idx][:_destroy] != nil
+          next
+        end
 
-      if url == ""
-        ret_link_params[:one_links_attributes][idx][:_destroy] = 1
-      end
-    }
+        if url == ""
+          ret_link_params[:one_links_attributes][idx][:_destroy] = 1
+        end
+      }
     end
     @link_new = Link.new(ret_link_params)
 
@@ -34,12 +34,15 @@ class LinksController < ApplicationController
 
     @link_new.one_links.each{ |one_link|
       url = one_link.url
+      url = encode_ja(url)
+      binding.pry
+      if url.slice(0..6) == "http://" or url.slice(0..7) == "https://"
 
-      if url == ""
-        one_link.url = "#"
-        one_link.url_title = "URLが空です"
-        next
+      else
+        url = "https://" + url
       end
+
+
       charset = nil
       begin
         # ここの部分をキャッシュの有無で場合分けしたい
@@ -97,6 +100,8 @@ class LinksController < ApplicationController
   end
 
 
+
+
   # patch '/:user_name/themes/:theme_hashid/edit/:link_hashid' => 'links#update', as: 'update_theme_link'
   # スクレイピングをしてOGPを抜き出し、Linkを更新
   def update
@@ -106,50 +111,51 @@ class LinksController < ApplicationController
     ret_link_params = link_params
     # binding.pry
     if ret_link_params[:one_links_attributes].blank? == false
-    ret_link_params[:one_links_attributes].keys.each { |idx|
-      url = ret_link_params[:one_links_attributes][idx][:url]
-      if ret_link_params[:one_links_attributes][idx][:_destroy] != nil
-        next
-      end
-
-      if url == ""
-        ret_link_params[:one_links_attributes][idx][:_destroy] = 1
-        next
-      end
-      charset = nil
-      begin
-        # ここの部分をキャッシュの有無で場合分けしたい
-        html = OpenURI.open_uri(url) do |f|
-          charset = f.charset # 文字種別を取得
-          f.read # htmlを読み込んで変数htmlに渡す
+      ret_link_params[:one_links_attributes].keys.each { |idx|
+        url = ret_link_params[:one_links_attributes][idx][:url]
+        if ret_link_params[:one_links_attributes][idx][:_destroy] != nil
+          next
         end
-      rescue
-        ret_link_params[:one_links_attributes][idx][:url_title] = "不正なURLです"
-      else
-        # ノコギリを使ってhtmlを解析
-        doc = Nokogiri::HTML.parse(html, charset)
 
-        # title
-        if doc.css('//meta[property="og:title"]/@content').empty?
-          ret_link_params[:one_links_attributes][idx][:url_title] = doc.title.to_s
+        if url == ""
+          ret_link_params[:one_links_attributes][idx][:_destroy] = 1
+          next
+        end
+        url = encode_ja(url)
+        charset = nil
+        begin
+          # ここの部分をキャッシュの有無で場合分けしたい
+          html = OpenURI.open_uri(url) do |f|
+            charset = f.charset # 文字種別を取得
+            f.read # htmlを読み込んで変数htmlに渡す
+          end
+        rescue
+          ret_link_params[:one_links_attributes][idx][:url_title] = "不正なURLです"
         else
-          ret_link_params[:one_links_attributes][idx][:url_title] = doc.css('//meta[property="og:title"]/@content').to_s
-        end
+          # ノコギリを使ってhtmlを解析
+          doc = Nokogiri::HTML.parse(html, charset)
 
-        if ret_link_params[:one_links_attributes][idx][:url_title] == ""
-          ret_link_params[:one_links_attributes][idx][:url_title] = "タイトル未設定"
-        end
+          # title
+          if doc.css('//meta[property="og:title"]/@content').empty?
+            ret_link_params[:one_links_attributes][idx][:url_title] = doc.title.to_s
+          else
+            ret_link_params[:one_links_attributes][idx][:url_title] = doc.css('//meta[property="og:title"]/@content').to_s
+          end
 
-        # description
-        if doc.css('//meta[property="og:description"]/@content').empty?
-          ret_link_params[:one_links_attributes][idx][:url_description] = doc.css('//meta[name$="escription"]/@content').to_s
-        else
-          ret_link_params[:one_links_attributes][idx][:url_description] = doc.css('//meta[property="og:description"]/@content').to_s
-        end
+          if ret_link_params[:one_links_attributes][idx][:url_title] == ""
+            ret_link_params[:one_links_attributes][idx][:url_title] = "タイトル未設定"
+          end
 
-        ret_link_params[:one_links_attributes][idx][:url_image] = doc.css('//meta[property="og:image"]/@content').to_s
-      end
-    }
+          # description
+          if doc.css('//meta[property="og:description"]/@content').empty?
+            ret_link_params[:one_links_attributes][idx][:url_description] = doc.css('//meta[name$="escription"]/@content').to_s
+          else
+            ret_link_params[:one_links_attributes][idx][:url_description] = doc.css('//meta[property="og:description"]/@content').to_s
+          end
+
+          ret_link_params[:one_links_attributes][idx][:url_image] = doc.css('//meta[property="og:image"]/@content').to_s
+        end
+      }
     end
 
     respond_to do |format|
@@ -187,6 +193,20 @@ class LinksController < ApplicationController
     # 投稿時、Linkとそれに結びついたデータをコントローラに通す
     def link_params
       params.require(:link).permit(:subtitle, :caption, :theme_id, :theme_hashid, one_links_attributes: [:id, :link_id, :url, :_destroy])
+    end
+
+    # 日本語のみエンコード
+    # 参考：https://www.mk-mode.com/blog/2013/08/15/ruby-url-encode-only-japanese/
+    def encode_ja(url)
+      ret = ""
+      url.split(//).each do |c|
+        if  /[-_.!~*'()a-zA-Z0-9;\/\?:@&=+$,%#]/ =~ c
+          ret.concat(c)
+        else
+          ret.concat(CGI.escape(c))
+        end
+      end
+      return ret
     end
 
 end
