23d7f617732227b91c5d7a64ff17bccbf6ed41c9
EC2 Default User <ec2-user@ip-172-31-19-11.ec2.internal>
2022-07-15 17:38:11 +0000

    :bug:

--- app/controllers/tags_controller.rb
+++
@@ -1,9 +0,0 @@
-class TagsController < ApplicationController
-  def index
-    @tag_all = Theme.tag_counts
-  end
-
-  def show
-    @theme_tagged_with = Theme.tagged_with(params[:tag_name])
-  end
-end
