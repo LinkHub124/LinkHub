b5e77a61f6f4eef99d0c83548ec5277ba8b6e724
EC2 Default User <ec2-user@ip-172-31-15-242.ec2.internal>
2022-08-09 18:34:47 +0000

    :bug: production.rbに追加記述

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,20 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.perform_caching = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:                 587,
+    address:              'smtp.gmail.com',
+    domain:               'smtp.gmail.com',
+    user_name:            ENV['GOOGLE_MAIL_ADDRESS'],
+    password:             ENV['GOOGLE_MAILER_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+  config.web_console.whitelisted_ips = '0.0.0.0/0'
 end
