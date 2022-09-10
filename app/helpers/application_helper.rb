43bd227565e0bd489919b43eeca3b1fe2497a202
EC2 Default User <ec2-user@ip-172-31-15-242.ec2.internal>
2022-09-10 00:12:06 +0000

    :art: OGPの修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
     if page_title.empty?
       base_title
     else
-      base_title + " | " + page_title
+      page_title + " | " + base_title
     end
   end
   
@@ -21,23 +21,23 @@ module ApplicationHelper
   
   
   
-  def og_description(page_description = '')
-    base_description = "LinkHubはリンク集の共有をするサイトです。日常で得た知見、役立つ情報を他のユーザーたちと共有していきましょう。"
-    if page_description.empty?
-      base_description
-    else
-      base_description
-    end
-  end
+  # def og_description(page_description = '')
+  #   base_description = "LinkHubはリンク集の共有をするサイトです。日常で得た知見、役立つ情報を他のユーザーたちと共有していきましょう。"
+  #   if page_description.empty?
+  #     base_description
+  #   else
+  #     base_description
+  #   end
+  # end
   
   
   
-  def og_image(page_image = '')
-    base_image = "LinkHub.jpg"
-    if page_image.empty?
-      base_image
-    else
-      base_image
-    end
-  end
+  # def og_image(page_image = '')
+  #   base_image = "LinkHub.jpg"
+  #   if page_image.empty?
+  #     base_image
+  #   else
+  #     base_image
+  #   end
+  # end
 end
