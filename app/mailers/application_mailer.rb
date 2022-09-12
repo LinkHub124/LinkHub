d7a0a5e47162778456bb6d710adedc610b5060de
EC2 Default User <ec2-user@ip-172-31-15-242.ec2.internal>
2022-09-12 12:06:42 +0000

    :art: 文字の大きさ、エラー文を日本語にするなど

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,7 @@
 class ApplicationMailer < ActionMailer::Base
   # default from: 'from@example.com'
   # layout 'mailer'
-  default from:     ENV['GOOGLE_MAIL_ADDRESS'],
+  default from: "LinkHub<#{ENV['GOOGLE_MAIL_ADDRESS']}>",
           cc:       ENV['GOOGLE_MAIL_ADDRESS']
   layout 'mailer'
 end
