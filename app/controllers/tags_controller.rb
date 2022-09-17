c7ef7369d993fc3711264c7a3d655f03aeab91ee
EC2 Default User <ec2-user@ip-172-31-15-242.ec2.internal>
2022-09-17 01:47:54 +0000

    :bug: tag検索で非公開でも見えるように

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -7,7 +7,11 @@ class TagsController < ApplicationController
   def show
     @tag_name = params[:tag_name]
     @theme_tagged_with = Theme.tagged_with(params[:tag_name])
-    @theme_tagged_with = @theme_tagged_with.select { |theme| theme.post_status == 2 }
+    if user_signed_in?
+      @theme_tagged_with = @theme_tagged_with.select { |theme| theme.post_status == 2 or theme.user == current_user }
+    else
+      @theme_tagged_with = @theme_tagged_with.select { |theme| theme.post_status == 2 }
+    end
     @theme_tagged_with = @theme_tagged_with.reverse
     @theme_tagged_with = Kaminari.paginate_array(@theme_tagged_with).page(params[:page]).per(10)
   end
