50466c60e2e2735a4a9bdb08fb58e31888a829d9
nope0124 <kami-ryo0124@ezweb.ne.jp>
2023-03-25 11:08:41 +0000

    テスト

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2022_11_28_162048) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
