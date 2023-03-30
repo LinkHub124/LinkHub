4dc58199e89b88dbe6931dd5d1a2653329b36370
nope0124 <kami-ryo0124@ezweb.ne.jp>
2023-03-31 02:29:03 +0900

    Favoritesコントローラのリファクタリング

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,32 +1,41 @@
 class FavoritesController < ApplicationController
   before_action :authenticate_user!, only: [:create, :destroy]
   include ActiveRecord::Sanitization::ClassMethods
+  MAX_FAVORITES_PER_PAGE = 10
 
-  # get '/:user_name/favorites' => 'favorites#index', as: 'user_favorites'
-  # いいね順にソートして表示
+  # GET '/:user_name/favorites' => 'favorites#index', as: 'user_favorites'
+  # Description: いいね順にソートして表示.
+  # Response:
+  # @user => 詳細ページのユーザーを取得.
+  # @theme_all => 表示させるいいね一覧を取得.
   def index
     @user = User.find_by(name: params[:user_name])
     @favorite_all = @user.favorites.select { |favorite| favorite.theme.post_status == 2 }
 
+    # クエリパラメータにtheme_query(:tq)がある場合、検索処理をする.
     if params[:tq]
       query = params[:tq]
       @favorite_all = search_favorites(@favorite_all, query)
     end
 
-    @favorite_all = @favorite_all.reverse
-    @favorite_all = Kaminari.paginate_array(@favorite_all).page(params[:page]).per(10)
+    @favorite_all = @favorite_all.reverse # 降順に表示.
+    @favorite_all = Kaminari.paginate_array(@favorite_all).page(params[:page]).per(MAX_FAVORITES_PER_PAGE)
   end
 
-  # post '/:user_name/themes/:theme_hashid/favorites' => 'favorites#create', as: 'theme_favorites'
-  # いいねを保存
+  # POST '/:user_name/themes/:theme_hashid/favorites' => 'favorites#create', as: 'theme_favorites'
+  # Description: いいねを保存.
+  # Response:
+  # @theme => いいねを保存するテーマを取得.
   def create
     @theme = Theme.find(params[:theme_hashid])
     favorite = current_user.favorites.new(theme_id: @theme.id)
     favorite.save
   end
 
-  # delete '/:user_name/themes/:theme_hashid/favorites' => 'favorites#destroy'
-  # いいねを削除
+  # DELETE '/:user_name/themes/:theme_hashid/favorites' => 'favorites#destroy'
+  # Description: いいねを削除.
+  # Response:
+  # @theme => いいねを削除するテーマを取得.
   def destroy
     @theme = Theme.find(params[:theme_hashid])
     favorite = current_user.favorites.find_by(theme_id: @theme.id)
@@ -35,7 +44,7 @@ class FavoritesController < ApplicationController
 
   private
 
-  # 自分のいいねの検索
+  # Description: 自分のいいねの検索.
   def search_favorites(favorite_all, search_text)
     favorite_searched = []
     favorite_all.each do |favorite|
