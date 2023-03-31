2a1096a0d406bb6b0db0bc641885aa2062576953
nope0124 <kami-ryo0124@ezweb.ne.jp>
2023-04-01 02:12:29 +0900

    Linkモデルのリファクタリング

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,15 +1,16 @@
 class Link < ApplicationRecord
-  belongs_to :user
+  include Hashid::Rails
+
+  # Association
   belongs_to :theme
-
   has_many :one_links, -> { order(position: :asc) }, dependent: :destroy
+  accepts_nested_attributes_for :one_links, allow_destroy: true
 
+  # Validation
   validates :subtitle, presence: true, length: { maximum: 50 }
   validates :caption, length: { maximum: 500 }
 
-  accepts_nested_attributes_for :one_links, allow_destroy: true # この行を追記
-
-  include Hashid::Rails
+  # Description: captionかsubtitleにwordを含むリンク集を返す.
   def self.looks(word)
     link  = Link.where('caption LIKE?', "%#{word}%")
     link += Link.where('subtitle LIKE?', "%#{word}%")
