4e626e88dd5023f6c2df0a862b176967e592cbb5
nope0124 <kami-ryo0124@ezweb.ne.jp>
2023-06-01 20:30:40 +0900

    コメントを日本語に変更

--- spec/requests/themes_request_spec.rb
+++ spec/requests/themes_request_spec.rb
@@ -10,14 +10,14 @@ RSpec.describe 'Themesコントローラーのテスト', type: :request do
   let!(:limited_theme2)  { create(:theme, title: 'Limited theme by user2', user: user2, post_status: 1) }
   let!(:released_theme2) { create(:theme, title: 'Released theme by user2', user: user2, post_status: 2) }
 
-  describe 'user1 login' do
+  describe 'user1でログイン' do
     before do
       user1.confirm
       sign_in user1
     end
 
     describe 'GET #index_follow' do
-      it 'returns HTTP status as 200 OK' do
+      it '200 OKを返し、フォローした人の全体公開テーマが表示' do
         user1.follow(user2.name)
         get index_follow_path
         expect(response).to have_http_status(:success)
@@ -31,7 +31,7 @@ RSpec.describe 'Themesコントローラーのテスト', type: :request do
     end
 
     describe 'GET #index' do
-      it 'returns HTTP status as 200 OK' do
+      it '200 OKを返し、全ユーザの全体公開テーマが表示' do
         get root_path
         expect(response).to have_http_status(:success)
         expect(response.body).not_to include('Private theme by user1')
@@ -44,68 +44,68 @@ RSpec.describe 'Themesコントローラーのテスト', type: :request do
     end
 
     describe 'POST #create' do
-      it 'returns HTTP status as 302 Redirect' do
+      it '302 Redirectを返す' do
         post themes_path(user_name: user1.name), params: { theme: { title: 'test1' } }
         expect(response).to have_http_status(:redirect)
       end
 
-      it 'returns HTTP status as 302 Redirect' do
+      it '302 Redirectを返す' do
         post themes_path(user_name: user2.name), params: { theme: { title: 'test2' } }
         expect(response).to have_http_status(:redirect)
       end
     end
 
     describe 'GET #show' do
-      context 'access private_theme1' do
-        it 'returns HTTP status as 200 OK' do
+      context 'private_theme1にアクセス' do
+        it '200 OKを返し、非公開テーマを閲覧できる' do
           get theme_path(user_name: user1.name, theme_hashid: private_theme1.hashid)
           expect(response).to have_http_status(:success)
           expect(response.body).to include('Private theme by user1')
         end
       end
 
-      context 'access limited_theme1' do
-        it 'returns HTTP status as 200 OK' do
+      context 'limited_theme1にアクセス' do
+        it '200 OKを返し、限定公開テーマが閲覧できる' do
           get theme_path(user_name: user1.name, theme_hashid: limited_theme1.hashid)
           expect(response).to have_http_status(:success)
           expect(response.body).to include('Limited theme by user1')
         end
       end
 
-      context 'access released_theme1' do
-        it 'returns HTTP status as 200 OK' do
+      context 'released_theme1にアクセス' do
+        it '200 OKを返し、全体公開テーマが閲覧できる' do
           get theme_path(user_name: user1.name, theme_hashid: released_theme1.hashid)
           expect(response).to have_http_status(:success)
           expect(response.body).to include('Released theme by user1')
         end
       end
 
-      context 'access private_theme2' do
-        it 'returns HTTP status as 404 Not Found' do
+      context 'private_theme2にアクセス' do
+        it '404 Not Foundを返す' do
           get theme_path(user_name: user2.name, theme_hashid: private_theme2.hashid)
           expect(response).to have_http_status(:not_found)
           expect(response.body).not_to include('Private theme by user2')
         end
       end
 
-      context 'access limited_theme2' do
-        it 'returns HTTP status as 200 OK' do
+      context 'limited_theme2にアクセス' do
+        it '200 OKを返し、別ユーザの限定公開テーマが閲覧できる' do
           get theme_path(user_name: user2.name, theme_hashid: limited_theme2.hashid)
           expect(response).to have_http_status(:success)
           expect(response.body).to include('Limited theme by user2')
         end
       end
 
-      context 'access released_theme2' do
-        it 'returns HTTP status as 200 OK' do
+      context 'released_theme2にアクセス' do
+        it '200 OKを返し、別ユーザの全体公開テーマが閲覧できる' do
           get theme_path(user_name: user2.name, theme_hashid: released_theme2.hashid)
           expect(response).to have_http_status(:success)
           expect(response.body).to include('Released theme by user2')
         end
       end
 
-      context 'access non-existent theme' do
-        it 'returns HTTP status as 404 Not Found' do
+      context '存在しないthemeにアクセス' do
+        it '404 Not Foundを返す' do
           get theme_path(user_name: user1.name, theme_hashid: released_theme2.hashid)
           expect(response).to have_http_status(:not_found)
         end
@@ -113,23 +113,23 @@ RSpec.describe 'Themesコントローラーのテスト', type: :request do
     end
 
     describe 'GET #edit' do
-      context 'posting user matches current_user' do
-        it 'returns HTTP status as 200 OK' do
+      context '投稿ユーザーとログインユーザーが一致' do
+        it '200 OKを返し、編集画面に遷移できる' do
           get edit_theme_path(user_name: user1.name, theme_hashid: released_theme1.hashid)
           expect(response).to have_http_status(:success)
           expect(response.body).to include('Released theme by user1')
         end
       end
 
-      context 'posting user does not match current_user' do
-        it 'returns HTTP status as 404 Not Found' do
+      context '投稿ユーザーとログインユーザーが不一致' do
+        it '404 Not Foundを返す' do
           get edit_theme_path(user_name: user2.name, theme_hashid: released_theme2.hashid)
           expect(response).to have_http_status(:not_found)
         end
       end
 
-      context 'access non-existent theme' do
-        it 'returns HTTP status as 404 Not Found' do
+      context '存在しないthemeにアクセス' do
+        it '404 Not Foundを返す' do
           get edit_theme_path(user_name: user1.name, theme_hashid: released_theme2.hashid)
           expect(response).to have_http_status(:not_found)
         end
@@ -137,29 +137,29 @@ RSpec.describe 'Themesコントローラーのテスト', type: :request do
     end
 
     describe 'PATCH #update' do
-      context 'posting user matches current_user' do
-        it 'returns HTTP status as 302 Redirect' do
+      context '投稿ユーザーとログインユーザーが一致' do
+        it '302 Redirectを返す' do
           patch update_theme_path(user_name: user1.name, theme_hashid: released_theme1.hashid), params: { theme: { title: 'test1', post_status: 0 } }
           expect(response).to have_http_status(:redirect)
         end
       end
 
-      context 'posting user does not match current_user' do
-        it 'returns HTTP status as 404 Not Found' do
+      context '投稿ユーザーとログインユーザーが不一致' do
+        it '404 Not Foundを返す' do
           patch update_theme_path(user_name: user2.name, theme_hashid: released_theme2.hashid), params: { theme: { title: 'test2', post_status: 0 } }
           expect(response).to have_http_status(:not_found)
         end
       end
 
-      context 'access non-existent theme' do
-        it 'returns HTTP status as 404 Not Found' do
+      context '存在しないthemeにアクセス' do
+        it '404 Not Foundを返す' do
           patch update_theme_path(user_name: user1.name, theme_hashid: released_theme2.hashid), params: { theme: { title: 'test3', post_status: 0 } }
           expect(response).to have_http_status(:not_found)
         end
       end
 
-      context 'posting user matches current_user and validation errors occur' do
-        it 'returns HTTP status as 422 Unprocessable Entity' do
+      context '投稿ユーザーとログインユーザーが一致かつ、バリデーションエラーが発生' do
+        it '422 Unprocessable Entityを返す' do
           patch update_theme_path(user_name: user1.name, theme_hashid: released_theme1.hashid), params: { theme: { title: '', post_status: 0 } }
           expect(response).to have_http_status(:unprocessable_entity)
         end
@@ -167,22 +167,22 @@ RSpec.describe 'Themesコントローラーのテスト', type: :request do
     end
 
     describe 'DELETE #destroy' do
-      context 'posting user matches current_user' do
-        it 'returns HTTP status as 302 Redirect' do
+      context '投稿ユーザーとログインユーザーが一致' do
+        it '302 Redirectを返す' do
           delete destroy_theme_path(user_name: user1.name, theme_hashid: released_theme1.hashid)
           expect(response).to have_http_status(:redirect)
         end
       end
 
-      context 'posting user does not match current_user' do
-        it 'returns HTTP status as 404 Not Found' do
+      context '投稿ユーザーとログインユーザーが不一致' do
+        it '404 Not Foundを返す' do
           delete destroy_theme_path(user_name: user2.name, theme_hashid: released_theme2.hashid)
           expect(response).to have_http_status(:not_found)
         end
       end
 
-      context 'access non-existent theme' do
-        it 'returns HTTP status as 404 Not Found' do
+      context '存在しないthemeにアクセス' do
+        it '404 Not Foundを返す' do
           delete destroy_theme_path(user_name: user1.name, theme_hashid: released_theme2.hashid)
           expect(response).to have_http_status(:not_found)
         end
@@ -190,16 +190,16 @@ RSpec.describe 'Themesコントローラーのテスト', type: :request do
     end
   end
 
-  describe 'not login' do
+  describe '未ログイン' do
     context 'GET #index_follow' do
-      it 'returns HTTP status as 302 Redirect' do
+      it '302 Redirectを返す' do
         get index_follow_path
         expect(response).to have_http_status(:redirect)
       end
     end
 
     describe 'GET #index' do
-      it 'returns HTTP status as 200 OK' do
+      it '200 OKを返し、全ユーザの全体公開テーマが表示' do
         get root_path
         expect(response).to have_http_status(:success)
         expect(response.body).not_to include('Private theme by user1')
@@ -212,63 +212,63 @@ RSpec.describe 'Themesコントローラーのテスト', type: :request do
     end
 
     describe 'POST #create' do
-      it 'returns HTTP status as 302 Redirect' do
+      it '302 Redirectを返す' do
         post themes_path(user_name: user1.name), params: { theme: { title: 'test1' } }
         expect(response).to have_http_status(:redirect)
       end
     end
 
     describe 'GET #show' do
-      context 'access private_theme1' do
-        it 'returns HTTP status as 200 OK' do
+      context 'private_theme1にアクセス' do
+        it '404 Not Foundを返す' do
           get theme_path(user_name: user1.name, theme_hashid: private_theme1.hashid)
           expect(response).to have_http_status(:not_found)
           expect(response.body).not_to include('Private theme by user1')
         end
       end
 
-      context 'access limited_theme1' do
-        it 'returns HTTP status as 200 OK' do
+      context 'limited_theme1にアクセス' do
+        it '200 OKを返し、限定公開テーマが閲覧できる' do
           get theme_path(user_name: user1.name, theme_hashid: limited_theme1.hashid)
           expect(response).to have_http_status(:success)
           expect(response.body).to include('Limited theme by user1')
         end
       end
 
-      context 'access released_theme1' do
-        it 'returns HTTP status as 200 OK' do
+      context 'released_theme1にアクセス' do
+        it '200 OKを返し、全体公開テーマが閲覧できる' do
           get theme_path(user_name: user1.name, theme_hashid: released_theme1.hashid)
           expect(response).to have_http_status(:success)
           expect(response.body).to include('Released theme by user1')
         end
       end
 
-      context 'access private_theme2' do
-        it 'returns HTTP status as 404 Not Found' do
+      context 'private_theme2にアクセス' do
+        it '404 Not Foundを返す' do
           get theme_path(user_name: user2.name, theme_hashid: private_theme2.hashid)
           expect(response).to have_http_status(:not_found)
           expect(response.body).not_to include('Private theme by user2')
         end
       end
 
-      context 'access limited_theme2' do
-        it 'returns HTTP status as 200 OK' do
+      context 'limited_theme2にアクセス' do
+        it '200 OKを返し、限定公開テーマが閲覧できる' do
           get theme_path(user_name: user2.name, theme_hashid: limited_theme2.hashid)
           expect(response).to have_http_status(:success)
           expect(response.body).to include('Limited theme by user2')
         end
       end
 
-      context 'access released_theme2' do
-        it 'returns HTTP status as 200 OK' do
+      context 'released_theme2にアクセス' do
+        it '200 OKを返し、全体公開テーマが閲覧できる' do
           get theme_path(user_name: user2.name, theme_hashid: released_theme2.hashid)
           expect(response).to have_http_status(:success)
           expect(response.body).to include('Released theme by user2')
         end
       end
 
-      context 'access non-existent theme' do
-        it 'returns HTTP status as 404 Not Found' do
+      context '存在しないthemeにアクセス' do
+        it '404 Not Foundを返す' do
           get theme_path(user_name: user1.name, theme_hashid: released_theme2.hashid)
           expect(response).to have_http_status(:not_found)
         end
@@ -276,15 +276,15 @@ RSpec.describe 'Themesコントローラーのテスト', type: :request do
     end
 
     describe 'GET #edit' do
-      context 'access user1 theme' do
-        it 'returns HTTP status as 404 Not Found' do
+      context 'user1の投稿テーマ編集画面にアクセス' do
+        it '404 Not Foundを返す' do
           get edit_theme_path(user_name: user1.name, theme_hashid: released_theme1.hashid)
           expect(response).to have_http_status(:not_found)
         end
       end
 
-      context 'access non-existent theme' do
-        it 'returns HTTP status as 404 Not Found' do
+      context '存在しないthemeにアクセス' do
+        it '404 Not Foundを返す' do
           get edit_theme_path(user_name: user1.name, theme_hashid: released_theme2.hashid)
           expect(response).to have_http_status(:not_found)
         end
@@ -292,15 +292,15 @@ RSpec.describe 'Themesコントローラーのテスト', type: :request do
     end
 
     describe 'PATCH #update' do
-      context 'access user1 theme' do
-        it 'returns HTTP status as 404 Redirect' do
+      context 'user1の投稿テーマを更新する' do
+        it '404 Not Foundを返す' do
           patch update_theme_path(user_name: user1.name, theme_hashid: released_theme1.hashid), params: { theme: { title: 'test1', post_status: 0 } }
           expect(response).to have_http_status(:not_found)
         end
       end
 
-      context 'access non-existent theme' do
-        it 'returns HTTP status as 404 Not Found' do
+      context '存在しないthemeにアクセス' do
+        it '404 Not Foundを返す' do
           patch update_theme_path(user_name: user1.name, theme_hashid: released_theme2.hashid), params: { theme: { title: 'test3', post_status: 0 } }
           expect(response).to have_http_status(:not_found)
         end
@@ -308,15 +308,15 @@ RSpec.describe 'Themesコントローラーのテスト', type: :request do
     end
 
     describe 'DELETE #destroy' do
-      context 'access user1 theme' do
-        it 'returns HTTP status as 404 Not Found' do
+      context 'user1の投稿テーマを削除する' do
+        it '404 Not Foundを返す' do
           delete destroy_theme_path(user_name: user1.name, theme_hashid: released_theme1.hashid)
           expect(response).to have_http_status(:not_found)
         end
       end
 
-      context 'access non-existent theme' do
-        it 'returns HTTP status as 404 Not Found' do
+      context '存在しないthemeにアクセス' do
+        it '404 Not Foundを返す' do
           delete destroy_theme_path(user_name: user1.name, theme_hashid: released_theme2.hashid)
           expect(response).to have_http_status(:not_found)
         end
