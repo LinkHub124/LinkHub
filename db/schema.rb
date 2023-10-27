70b12801b97d21ab50ef93869ef70f7f83d76e3b
nope0124 <kami-ryo0124@ezweb.ne.jp>
2023-10-28 01:27:32 +0900

    openapi修正

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,19 @@
 
 ActiveRecord::Schema.define(version: 2023_06_03_044113) do
 
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "name", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "theme_id"
@@ -119,10 +132,11 @@ ActiveRecord::Schema.define(version: 2023_06_03_044113) do
     t.string "homepage_url"
     t.integer "score", default: 0
     t.boolean "is_deleted", default: false, null: false
-    t.string "provider", default: "email", null: false
-    t.string "uid", default: "", null: false
+    t.string "uid"
+    t.string "provider"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_admin", default: false
     t.text "tokens"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
